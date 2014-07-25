4ceaa155a77eb1f3a949b6eb9c20784cfe31258c
JayTeeSF <jonathan@his-service.net>
2014-07-25 09:03:49 -0700

    show generation of form-object params[:signup]

--- 3-form-objects/3_signups_controller.rb
+++ 3-form-objects/3_signups_controller.rb
@@ -1,11 +1,5 @@
 class SignupsController < ApplicationController
-  def create
-    @signup = Signup.new(params[:signup])
-
-    if @signup.save
-      redirect_to dashboard_path
-    else
-      render "new"
-    end
+  def new
+     @signup = Signup.new
   end
 end
